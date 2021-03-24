9f44d127123955d065654ca6faafd21fe274e8fe
Nazarikk <nazariy.kudryk1@gmail.com>
2021-03-24 13:41:46 +0200

    Add check_events and update_screen events functions to replace run_game

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,10 +22,15 @@ class AlienInvasion:
 
     def run_game(self):
         while True:
+            self._check_events()
+            self._update_screen()
+
+    def _check_events(self):
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
+               if event.type == pygame.QUIT:
+                   sys.exit()
+        
+    def _update_screen(self):
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
 
@@ -34,3 +39,4 @@ class AlienInvasion:
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
+
