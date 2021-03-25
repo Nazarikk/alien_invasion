8832a22e83d998be80e0f631a5e6be92385c2b7d
Nazarikk <nazariy.kudryk1@gmail.com>
2021-03-25 12:08:51 +0200

    Add speed variable

--- ship.py
+++ ship.py
@@ -4,13 +4,18 @@ class Ship:
     
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
-        # Add ship image
+        # Add ship 
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #Save horisontal ship position
+    
+        self.x = float(self.rect.x)
 
         #Movement indicator
         self.moving_right = False
@@ -21,10 +26,11 @@ class Ship:
         update player position based on movement indicator
         """
         if self.moving_right:
-            self.rect.x +=1
+            self.rect.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -=1
+            self.rect.x -= self.settings.ship_speed
 
+        #self.rect.x = self.x
             
     def blitme(self):
         self.screen.blit(self.image, self.rect)
